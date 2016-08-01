ab09e766d52b16df14ccb6959b74df1b681fe243
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-08-01 14:14:20 -0500

    if else more functions

--- public/js/if-else.js
+++ public/js/if-else.js
@@ -1,57 +1,56 @@
 "use strict";
-var grade1 = 70;
-var grade2 = 80;
-var grade3 = 95;
+var grades = [70,80,95];
 var averageGrades;
+var totalGrades=0;
+var minGrade=80;
 
-function getAverage(a,b,c){
-	var result = (a+b+c)/3;
-	result.toFixed(2);
-	return result;
+function getAverage(array, length){
+	for(var i=0; i< array.length; i++){
+		totalGrades += array[i]
+	}
+	return totalGrades/length;
 }
 function gradeMessage(average){
-	if(average>80){
-	console.log("you're awesome");
+	if(average>minGrade){
+	console.log("You're awesome");
 	}
 	else{
-		console.log("you need more practice");
+		console.log("You need more practice");
 	}
 }
 
-
-averageGrades = getAverage(grade1,grade2,grade3);
-console.log("The average is: "+ averageGrades);
-gradeMessage(averageGrades);
+averageGrades = getAverage(grades, grades.length);
+console.log("The average is: "+ averageGrades +
+	"\n"+gradeMessage(averageGrades));
 
 
 // end of first
-var name1 = "Cameron";
-var cameron = 180;
-var name2 = "Ryan";
-var ryan = 250;
-var name3 = "George";
-var george = 320;
+var arrayNames = ['Cameron','Ryan','George'];
+var values = [180,250,320];
+var discount = 0.35;
+var minAmountForDiscount = 200;
+var decimalValues=2;
 
 function fixMoney(amount){
-	amount = amount.toFixed(2);
+	amount = amount.toFixed(decimalValues);
 	return amount;
 }
 
 function getDiscount(name, moneySpent){
-	if(moneySpent>200){
- 		console.log(name+" bought $"+moneySpent+", discount was applied. Final payment: $"+(moneySpent-moneySpent*.35));
+	if(moneySpent>minAmountForDiscount){
+ 		console.log(name+" bought $"+moneySpent+", discount was applied. Final payment: $"+fixMoney((moneySpent-moneySpent*discount)));
 	}
 	else{
 	 	console.log(name+" bought $"+moneySpent+", no discount was applied. Final payment: $"+moneySpent);
 	}
 }
-cameron = fixMoney(cameron);
-ryan = fixMoney(ryan);
-george = fixMoney(george);
-getDiscount(name1, cameron);
-getDiscount(name2, ryan);
-getDiscount(name3, george);
- 
+
+for(var i=0; i<values.length;i++){
+	values[i] = fixMoney(values[i]);
+	getDiscount(arrayNames[i], values[i]);
+}
+
+
 // end of second
 
 function doFlipACoin(){
