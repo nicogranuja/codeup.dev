d867b3613c6a3b9f2449316bdd6124860d04d7bc
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-09-13 15:43:07 -0500

    started working on the Auth file

---
+++ Auth.php
@@ -0,0 +1,30 @@
+<?php
+	require_once "../../exercises/Log.php";
+Class Auth {
+	// public static $password = "$2y$10$SLjwBwdOVvnMgWxvTI4Gb.YVcmDlPTpQystHMO2Kfyi/DS8rgA0Fm";
+	public function __construct()
+    {
+		// $password = "$2y$10$SLjwBwdOVvnMgWxvTI4Gb.YVcmDlPTpQystHMO2Kfyi/DS8rgA0Fm";
+    }
+	
+
+	public static function attempt($username, $password){
+
+	}
+
+
+	public static function check(){
+
+	}
+
+	public static function user(){
+		
+	}
+
+	public static function logout(){
+		
+	}
+
+
+}
+?>
